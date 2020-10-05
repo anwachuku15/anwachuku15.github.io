a3b34ab9af5b7acf0d0f738de44ccc2c822d6db4
anwachuku15 <anwachuku15@gmail.com>
2020-10-05 16:28:54 -0400

    added Navbar component

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      {/* <Nav>
+        <NavbarContainer>
+            <NavLogo>
+
+            </NavLogo>
+        </NavbarContainer>
+    </Nav> */}
+    </>
+  );
+};
+
+export default Navbar;
