fed5a40265924d4dfc3ac412373d9ca31944c4f0
anwachuku15 <anwachuku15@gmail.com>
2020-10-05 18:08:34 -0400

    cleanup

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import { FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import { Link as ScrollLink } from "react-scroll";
 
 export const SidebarContainer = styled.aside`
   position: fixed;
