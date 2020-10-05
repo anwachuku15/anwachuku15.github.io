a5654e9402e9403eb1088151265bd619ad331b3f
anwachuku15 <anwachuku15@gmail.com>
2020-10-05 16:09:04 -0400

    Updates

---
+++ precache-manifest.399aebcec36a681869aa3c0dc9886ad7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd3c0271d72090aafb29198adc3d98a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dcd192a87cfd7979fbf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb734ef84da90fc99924",
+    "url": "/static/js/2.70addb30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70addb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dcd192a87cfd7979fbf",
+    "url": "/static/js/main.87c70d96.chunk.js"
+  },
+  {
+    "revision": "4eb6c465c2efef3f23dc",
+    "url": "/static/js/runtime-main.cd133b2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
