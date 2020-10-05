383c628c0c72e060fb55c5f4ff8553231c910cc2
anwachuku15 <anwachuku15@gmail.com>
2020-10-05 19:10:39 -0400

    added Home page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import ResponsiveNavbar from "./components/ResponsiveNavbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <ResponsiveNavbar />
-        {/* <Switch>
-      <Route exact path="/" component={Home} />
-      </Switch> */}
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
       </div>
     </Router>
   );
