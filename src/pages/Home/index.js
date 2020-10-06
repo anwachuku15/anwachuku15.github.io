63c4c21149f85be299e4764eff387ef2e6353c00
anwachuku15 <anwachuku15@gmail.com>
2020-10-05 23:10:44 -0400

    added object prop to AboutSection

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import AboutSection from "../../components/AboutSection";
+import { object1 } from "../../components/AboutSection/Data";
 import HeroSection from "../../components/HeroSection";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <AboutSection {...object1} />
     </>
   );
 };
