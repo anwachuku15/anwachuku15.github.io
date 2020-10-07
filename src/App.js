a1403acaa353fd97df7a31b81c3181dbf0b71801
anwachuku15 <anwachuku15@gmail.com>
2020-10-07 17:20:40 -0400

    added Footer component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ResponsiveNavbar from "./components/ResponsiveNavbar";
 import Home from "./pages/Home";
 import Project from "./pages/Project";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route exact path="/" component={Home} />
           <Route exact path="/:project" component={Project} />
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
