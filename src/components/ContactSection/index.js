73189faaa2436845fb1a879a95361bd4595bac1d
anwachuku15 <anwachuku15@gmail.com>
2020-10-07 17:20:09 -0400

    created ContactSection component

---
+++ src/components/ContactSection/index.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Button from "../Button";
+import {
+  ContactContainer,
+  ContactWrapper,
+  ContactRow,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  ImgWrap,
+  Img,
+} from "./ContactElements";
+
+const ContactSection = ({
+  lightBg,
+  imgStart,
+  topline,
+  lightText,
+  heading,
+  description,
+  lightTextDesc,
+  darkText,
+  img,
+  alt,
+  dark,
+  primary,
+  buttonLabel,
+}) => {
+  const id = "id";
+  return (
+    <>
+      <ContactContainer lightBg={lightBg} id="contact">
+        <ContactWrapper>
+          <ContactRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{topline}</TopLine>
+                <Heading lightText={lightText}>{heading}</Heading>
+                <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                <BtnWrap>
+                  <Button
+                    title={buttonLabel}
+                    direction="down"
+                    scrollTo="portfolio"
+                    lightBg={lightBg}
+                    primary={primary}
+                    dark={dark}
+                  />
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt={alt} />
+              </ImgWrap>
+            </Column2>
+          </ContactRow>
+        </ContactWrapper>
+      </ContactContainer>
+    </>
+  );
+};
+
+export default ContactSection;
