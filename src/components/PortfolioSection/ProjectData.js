96802b116e6f8bf111c2e8bd9f8a527c7e3d7da4
anwachuku15 <anwachuku15@gmail.com>
2020-10-07 15:01:53 -0400

    added project data

---
+++ src/components/PortfolioSection/ProjectData.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    linkTo: "/spotify-clone",
+    name: "Spotify Clone",
+    description:
+      "A Spotify clone that allows Spotify Premium users to play music from up to 50 of their most recent playlists.",
+    url: "https://spotify-clone-ed27e.web.app/",
+    github: "https://github.com/anwachuku15/spotify-clone",
+    //   images: ,
+  },
+  {
+    linkTo: "/amazon-clone",
+    name: "Amazon Clone",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    url: "https://spotify-clone-ed27e.web.app/",
+    github: "https://github.com/anwachuku15/amazon-clone",
+    //   images: ,
+  },
+  {
+    linkTo: "/lnb-mobile-app",
+    name: "LNB",
+    description:
+      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur cupidatat norum.",
+    url: "https://spotify-clone-ed27e.web.app/",
+    github: "https://github.com/anwachuku15/LNB",
+    //   images: ,
+  },
+];
