996da342a575742e773b063c53b0cab07830a403
anwachuku15 <anwachuku15@gmail.com>
2020-10-07 17:19:36 -0400

    created Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  FooterContainer,
+  FooterLink,
+  FooterLinkItems,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterName,
+  FooterWrapper,
+  SocialMedia,
+  SocialMediaWrapper,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+} from "./FooterElements";
+import { animateScroll as scroll } from "react-scroll";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        {/* <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLink>LinkedIn</FooterLink>
+              <FooterLink>Github</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <FooterName>Andrew Nwachuku © 2020</FooterName> */}
+        <SocialMedia>
+          <SocialMediaWrapper>
+            <SocialLogo to="/" onClick={toggleHome}>
+              Andrew Nwachuku
+            </SocialLogo>
+            <WebsiteRights>Andrew Nwachuku © 2020</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink
+                href="https://www.linkedin.com/in/anwachuku15/"
+                target="_blank"
+                aria-label="LinkedIn"
+              >
+                <FaLinkedin />
+              </SocialIconLink>
+              <SocialIconLink
+                href="https://www.github.com/anwachuku15/"
+                target="_blank"
+                aria-label="Github"
+              >
+                <FaGithub />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrapper>
+        </SocialMedia>
+      </FooterWrapper>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
