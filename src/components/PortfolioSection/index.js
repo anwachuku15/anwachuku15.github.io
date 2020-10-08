f84eb0dad94ca9d84edf344c0f7ee3a4263198cf
anwachuku15 <anwachuku15@gmail.com>
2020-10-08 10:38:47 -0400

    commented out future Framer Motion code

--- src/components/PortfolioSection/index.js
+++ src/components/PortfolioSection/index.js
@@ -17,6 +17,28 @@ import {
 import { projects } from "./ProjectData";
 
 const PortfolioSection = () => {
+  // FRAMER ANIMATION
+  // const [selectedId, setSelectedId] = useState(null)
+
+  // <AnimateSharedLayout type="crossfade">
+  //   {items.map(item => (
+  //     <motion.div layoutId={item.id} onClick={() => setSelectedId(item.id)}>
+  //       <motion.h5>{item.subtitle}</motion.h5>
+  //       <motion.h2>{item.title}</motion.h2>
+  //     </motion.div>
+  //   ))}
+
+  //   <AnimatePresence>
+  //     {selectedId && (
+  //       <motion.div layoutId={selectedIdentifier}>
+  //         <motion.h5>{item.subtitle}</motion.h5>
+  //         <motion.h2>{item.title}</motion.h2>
+  //         <motion.button onClick={() => setSelectedId(null)} />
+  //       </motion.div>
+  //     )}
+  //   </AnimatePresence>
+  // </AnimateSharedLayout>
+
   return (
     <PortfolioContainer id="portfolio">
       <PortfolioH1>Projects</PortfolioH1>
