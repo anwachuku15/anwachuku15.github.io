6acbf8cff5d1888d3b370aef26a1ad810c3b775a
anwachuku15 <anwachuku15@gmail.com>
2020-10-08 21:32:42 -0400

    created modal

--- src/components/PortfolioSection/index.js
+++ src/components/PortfolioSection/index.js
@@ -1,8 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Icon1 from "../../assets/images/responsivewebapp.svg";
-import Icon2 from "../../assets/images/mobileinterface.svg";
-import Icon3 from "../../assets/images/database.svg";
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
 
 import {
   PortfolioContainer,
@@ -12,59 +10,107 @@ import {
   PortfolioIcon,
   PortfolioH2,
   PortfolioP,
+  ModalButtonsContainer,
+  ViewSiteButtonsContainer,
+  ViewSiteButtonWrapper,
+  ViewSiteButton,
+  GoToSiteIcon,
+  ExitButton,
+  SelectedIconContainer,
+  SelectedCard,
+  SelectedIcon,
+  SelectedH2,
+  SelectedP,
+  Overlay,
 } from "./PortfolioElements";
-
+import Button from "../Button";
 import { projects } from "./ProjectData";
+import LaunchIcon from "@material-ui/icons/Launch";
+import CloseIcon from "@material-ui/icons/Close";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const PortfolioSection = () => {
   // FRAMER ANIMATION
-  // const [selectedId, setSelectedId] = useState(null)
+  const [selectedId, setSelectedId] = useState(null);
 
-  // <AnimateSharedLayout type="crossfade">
-  //   {items.map(item => (
-  //     <motion.div layoutId={item.id} onClick={() => setSelectedId(item.id)}>
-  //       <motion.h5>{item.subtitle}</motion.h5>
-  //       <motion.h2>{item.title}</motion.h2>
-  //     </motion.div>
-  //   ))}
-
-  //   <AnimatePresence>
-  //     {selectedId && (
-  //       <motion.div layoutId={selectedIdentifier}>
-  //         <motion.h5>{item.subtitle}</motion.h5>
-  //         <motion.h2>{item.title}</motion.h2>
-  //         <motion.button onClick={() => setSelectedId(null)} />
-  //       </motion.div>
-  //     )}
-  //   </AnimatePresence>
-  // </AnimateSharedLayout>
+  const handleClick = (id) => {
+    // console.log(id);
+    setSelectedId(id);
+  };
 
   return (
     <PortfolioContainer id="portfolio">
       <PortfolioH1>Projects</PortfolioH1>
-      <PortfolioWrapper>
-        <Link to={`${projects[0].linkTo}`} style={{ textDecoration: "none" }}>
-          <PortfolioCard>
-            <PortfolioIcon src={Icon1} />
-            <PortfolioH2>{projects[0].name}</PortfolioH2>
-            <PortfolioP>{projects[0].description}</PortfolioP>
-          </PortfolioCard>
-        </Link>
-        <Link to={`${projects[1].linkTo}`} style={{ textDecoration: "none" }}>
-          <PortfolioCard>
-            <PortfolioIcon src={Icon2} />
-            <PortfolioH2>{projects[1].name}</PortfolioH2>
-            <PortfolioP>{projects[1].description}</PortfolioP>
-          </PortfolioCard>
-        </Link>
-        <Link to={`${projects[2].linkTo}`} style={{ textDecoration: "none" }}>
-          <PortfolioCard>
-            <PortfolioIcon src={Icon2} />
-            <PortfolioH2>{projects[2].name}</PortfolioH2>
-            <PortfolioP>{projects[2].description}</PortfolioP>
-          </PortfolioCard>
-        </Link>
-      </PortfolioWrapper>
+      <AnimateSharedLayout type="crossfade">
+        <PortfolioWrapper>
+          {projects.map((project) => (
+            <PortfolioCard
+              layoutId={project.id}
+              onClick={() => handleClick(project.id)}
+            >
+              <PortfolioIcon src={project.img} />
+              <PortfolioH2>{project.name}</PortfolioH2>
+              <Button
+                title="Learn More"
+                openModal={() => handleClick(project.id)}
+                lightBg={true}
+              />
+
+              {/* <PortfolioP>{project.description}</PortfolioP> */}
+            </PortfolioCard>
+          ))}
+        </PortfolioWrapper>
+        <AnimatePresence>
+          {selectedId && (
+            <>
+              <Overlay
+                onClick={() => setSelectedId(null)}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0, transition: { duration: 0.15 } }}
+                transition={{ duration: 0.2, delay: 0.15 }}
+              />
+
+              <SelectedCard
+                layoutId={selectedId}
+                // initial={{ maxHeight: 560, width: 700 }}
+                // animate={{maxHeight: 340, }}
+              >
+                <SelectedIcon src={projects[selectedId - 1].img} />
+                <SelectedH2>{projects[selectedId - 1].name}</SelectedH2>
+                <SelectedP>{projects[selectedId - 1].description}</SelectedP>
+                <ModalButtonsContainer>
+                  <ViewSiteButtonsContainer>
+                    {projects[selectedId - 1].name !== "LNB" && (
+                      <ViewSiteButtonWrapper
+                        href={projects[selectedId - 1].url}
+                        target="_blank"
+                      >
+                        <ViewSiteButton>View Site</ViewSiteButton>
+                        <GoToSiteIcon>
+                          <LaunchIcon fontSize="small" />
+                        </GoToSiteIcon>
+                      </ViewSiteButtonWrapper>
+                    )}
+                    <ViewSiteButtonWrapper
+                      href={projects[selectedId - 1].github}
+                      target="_blank"
+                    >
+                      <ViewSiteButton>Source Code</ViewSiteButton>
+                      <GoToSiteIcon>
+                        <GitHubIcon fontSize="small" />
+                      </GoToSiteIcon>
+                    </ViewSiteButtonWrapper>
+                  </ViewSiteButtonsContainer>
+                  <ExitButton onClick={() => setSelectedId(null)}>
+                    <CloseIcon fontSize="large" />
+                  </ExitButton>
+                </ModalButtonsContainer>
+              </SelectedCard>
+            </>
+          )}
+        </AnimatePresence>
+      </AnimateSharedLayout>
     </PortfolioContainer>
   );
 };
