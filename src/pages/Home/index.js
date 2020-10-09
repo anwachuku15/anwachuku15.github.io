07a892887e695a9a64fb13a52473d57503ae65fe
anwachuku15 <anwachuku15@gmail.com>
2020-10-08 22:13:36 -0400

    removed warnings

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import AboutSection from "../../components/AboutSection";
-import { object1, object2, object3 } from "../../components/AboutSection/Data";
+import { object1, object2 } from "../../components/AboutSection/Data";
 import ContactSection from "../../components/ContactSection";
 import HeroSection from "../../components/HeroSection";
 import PortfolioSection from "../../components/PortfolioSection";
