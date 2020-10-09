30c8386686a2abc5f73e7b38f8b58b17fcaf0255
anwachuku15 <anwachuku15@gmail.com>
2020-10-08 21:36:16 -0400

    added project data

--- src/components/PortfolioSection/ProjectData.js
+++ src/components/PortfolioSection/ProjectData.js
@@ -1,29 +1,51 @@
+import Icon0 from "../../assets/images/composemusic.svg";
+import Icon1 from "../../assets/images/sendgift.svg";
+import Icon2 from "../../assets/images/mobileinterface.svg";
+
 export const projects = [
   {
+    id: 1,
+    img: Icon0,
     linkTo: "/spotify-clone",
     name: "Spotify Clone",
     description:
       "A Spotify clone that allows Spotify Premium users to play music from up to 50 of their most recent playlists.",
     url: "https://spotify-clone-ed27e.web.app/",
     github: "https://github.com/anwachuku15/spotify-clone",
-    //   images: ,
+    images: [
+      "../../assets/images/composemusic.svg",
+      "../../assets/images/mediaplayer.svg",
+      "../../assets/images/playlist.svg",
+    ],
   },
   {
+    id: 2,
+    img: Icon1,
     linkTo: "/amazon-clone",
     name: "Amazon Clone",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    url: "https://spotify-clone-ed27e.web.app/",
+    url: "https://github.com/anwachuku15/amazon-clone",
     github: "https://github.com/anwachuku15/amazon-clone",
-    //   images: ,
+    images: [
+      "../../assets/images/sendgift.svg",
+      "../../assets/images/deliveries.svg",
+      "../../assets/images/orderdelivered.svg",
+    ],
   },
   {
+    id: 3,
+    img: Icon2,
     linkTo: "/lnb-mobile-app",
     name: "LNB",
     description:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur cupidatat norum.",
-    url: "https://spotify-clone-ed27e.web.app/",
+    url: "https://github.com/anwachuku15/LNB",
     github: "https://github.com/anwachuku15/LNB",
-    //   images: ,
+    images: [
+      "../../assets/images/mobileinterface.svg",
+      "../../assets/images/mobiledevelopment.svg",
+      "../../assets/images/mobileapp.svg",
+    ],
   },
 ];
