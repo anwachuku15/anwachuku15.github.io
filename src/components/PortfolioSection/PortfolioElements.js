8b042347f257a1ca1a8f1ebd0f60a4f8e19b9d47
anwachuku15 <anwachuku15@gmail.com>
2020-10-11 19:50:54 -0400

    added carousel styled component

--- src/components/PortfolioSection/PortfolioElements.js
+++ src/components/PortfolioSection/PortfolioElements.js
@@ -90,17 +90,18 @@ export const SelectedCard = styled(motion.div)`
 `;
 
 export const PortfolioIcon = styled(motion.img)`
-  /* height: 160px; */
   /* width: 160px; */
+  /* height: 160px; */
   /* margin-bottom: 10px; */
   width: 100%;
   height: 195px;
 `;
 
-export const SelectedIcon = styled(motion.img)`
-  /* height: 240px; */
+export const CarouselWrapper = styled.div`
+  display: flex;
   height: 70%;
   width: 90%;
+  position: relative;
   @media screen and (max-width: 740px) {
     width: 400px;
     height: 65%;
@@ -109,6 +110,72 @@ export const SelectedIcon = styled(motion.img)`
   @media screen and (max-width: 480px) {
     width: 360px;
     height: 60%;
+  }
+`;
+
+export const CarouselWindow = styled.div`
+  position: relative;
+  overflow: hidden;
+  /* display: block; */
+`;
+
+export const Carousel = styled(motion.div)`
+  display: flex;
+  position: relative;
+  left: -100%;
+  /* width: 10000px; */
+`;
+
+export const SelectedImage = styled(motion.img)`
+  width: 100%;
+  -webkit-user-drag: none;
+`;
+
+export const PrevButton = styled.div`
+  top: calc(50% - 20px);
+  position: absolute;
+  background: rgba(0, 0, 0, 0.3);
+  color: white;
+  border-radius: 30px;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 18px;
+  z-index: 2;
+  left: 10px;
+  transition: background 0.2s ease;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.6);
+  }
+`;
+
+export const NextButton = styled.div`
+  top: calc(50% - 20px);
+  position: absolute;
+  background: rgba(0, 0, 0, 0.3);
+  color: white;
+  border-radius: 30px;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  user-select: none;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 18px;
+  z-index: 2;
+  right: 10px;
+  transition: background 0.2s ease;
+
+  &:hover {
+    background: rgba(0, 0, 0, 0.6);
   }
 `;
 
@@ -129,6 +196,14 @@ export const PortfolioH2 = styled(motion.h2)`
   color: black !important;
 `;
 
+export const SelectedTextContent = styled(motion.div)`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+
 export const SelectedH2 = styled(motion.h2)`
   text-align: left;
   width: 100%;
@@ -140,7 +215,9 @@ export const PortfolioP = styled(motion.p)`
   color: black !important;
 `;
 
-export const SelectedP = styled(motion.p)``;
+export const SelectedP = styled(motion.p)`
+  text-align: left;
+`;
 
 export const ModalButtonsContainer = styled.div`
   display: flex;
