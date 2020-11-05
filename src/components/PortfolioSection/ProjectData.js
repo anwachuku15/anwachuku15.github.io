fdc99a9708ad8db7a6f0f87197152e4c585b2a19
anwachuku15 <anwachuku15@gmail.com>
2020-11-04 21:34:55 -0500

    added project images

--- src/components/PortfolioSection/ProjectData.js
+++ src/components/PortfolioSection/ProjectData.js
@@ -1,14 +1,14 @@
-// import composemusic from "../../assets/images/svgs/composemusic.svg";
-// import playlist from "../../assets/images/svgs/playlist.svg";
-// import mediaplayer from "../../assets/images/svgs/mediaplayer.svg";
 import spotify from "../../assets/images/spotify/spotifyclone.png";
 import rapcaviar from "../../assets/images/spotify/rapcaviar.png";
 import rapcaviar2 from "../../assets/images/spotify/rapcaviar2.png";
 
-import sendgift from "../../assets/images/svgs/sendgift.svg";
+import amazon from "../../assets/images/amazon/home.png";
+import cart from "../../assets/images/amazon/cart.png";
+import checkout from "../../assets/images/amazon/checkout.png";
+
+import facebook from "../../assets/images/facebook/facebook.png";
+
 import mobileinterface from "../../assets/images/svgs/mobileinterface.svg";
-import deliveries from "../../assets/images/svgs/deliveries.svg";
-import orderdelivered from "../../assets/images/svgs/orderdelivered.svg";
 import mobiledevelopment from "../../assets/images/svgs/mobiledevelopment.svg";
 import mobileapp from "../../assets/images/svgs/mobileapp.svg";
 
@@ -29,18 +29,28 @@ export const projects = [
     name: "Amazon Clone",
     description:
       "A full stack Amazon clone with full e-commerce functionality, Stripe payment processsing, complex data management with React Context API, and clean UI/UX",
-    images: [sendgift, deliveries, orderdelivered],
-    url: "https://github.com/anwachuku15/amazon-clone",
+    images: [amazon, cart, checkout],
+    url: "https://clone-7b53f.web.app/",
     github: "https://github.com/anwachuku15/amazon-clone",
   },
   {
     id: 3,
-    linkTo: "/lnb-mobile-app",
-    name: "LNB",
+    linkTo: "/facebook-clone",
+    name: "Facebook Clone",
     description:
       "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur cupidatat norum.",
-    images: [mobileinterface, mobiledevelopment, mobileapp],
-    url: "https://github.com/anwachuku15/LNB",
+    images: [facebook, mobiledevelopment, mobileapp],
+    url: "https://facebookclone-mern-c56cd.firebaseapp.com/",
     github: "https://github.com/anwachuku15/LNB",
   },
+  // {
+  //   id: 4,
+  //   linkTo: "/lnb-mobile-app",
+  //   name: "LNB",
+  //   description:
+  //     "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur cupidatat norum.",
+  //   images: [mobileinterface, mobiledevelopment, mobileapp],
+  //   url: "https://github.com/anwachuku15/LNB",
+  //   github: "https://github.com/anwachuku15/LNB",
+  // },
 ];
