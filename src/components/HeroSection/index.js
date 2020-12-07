e45e56eb5d0b5e4e49b4004edc1a6ce6791acb97
anwachuku15 <anwachuku15@gmail.com>
2020-12-07 16:46:25 -0500

    added animations to hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -9,6 +9,7 @@ import {
 } from "./HeroElements";
 import Button from "../Button";
 import Video from "../../assets/videos/herovideo.mp4";
+import ScrollAnimation from "react-animate-on-scroll";
 
 const HeroSection = () => {
   return (
@@ -17,29 +18,46 @@ const HeroSection = () => {
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
       <HeroContent>
-        <HeroMainText>
-          <HeroH1>Hello, I'm </HeroH1>
-          <HeroH1
-            style={{
-              color: "rgb(225 21 21)",
-              background: "rgba(255, 255, 255, 0.69)",
-              padding: "1px 5px",
-              borderRadius: 10,
-              display: "inline-block",
-              marginLeft: 10,
-            }}
-          >
-            Andrew Nwachuku
-          </HeroH1>
-          <HeroH1>.</HeroH1>
-        </HeroMainText>
-        <HeroH1>I'm a full-stack web developer.</HeroH1>
-        <Button
-          title="See my work!"
-          direction="down"
-          scrollTo="portfolio"
-          lightBg={false}
-        />
+        <ScrollAnimation animateIn="zoomIn" duration={1.5} animateOnce={true}>
+          <HeroMainText>
+            <HeroH1>Hello, I'm </HeroH1>
+            <HeroH1
+              style={{
+                color: "rgb(225 21 21)",
+                background: "rgba(255, 255, 255, 0.69)",
+                padding: "1px 5px",
+                borderRadius: 10,
+                display: "inline-block",
+                marginLeft: 10,
+              }}
+            >
+              Andrew Nwachuku
+            </HeroH1>
+            <HeroH1>.</HeroH1>
+          </HeroMainText>
+        </ScrollAnimation>
+        <ScrollAnimation
+          delay={100}
+          animateIn="zoomInLeft"
+          duration={1.5}
+          animateOnce={true}
+        >
+          <HeroH1>I'm a full-stack web developer.</HeroH1>
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="zoomIn"
+          delay={1300}
+          duration={1}
+          animateOnce={true}
+          style={{ alignSelf: "center" }}
+        >
+          <Button
+            title="See my work!"
+            direction="down"
+            scrollTo="portfolio"
+            lightBg={false}
+          />
+        </ScrollAnimation>
       </HeroContent>
     </HeroContainer>
   );
