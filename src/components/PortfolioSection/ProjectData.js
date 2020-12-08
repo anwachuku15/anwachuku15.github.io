b1352be773ce0a1100e76dfb968ee38f95ac8fb2
anwachuku15 <anwachuku15@gmail.com>
2020-12-07 21:07:27 -0500

    added lnb project data

--- src/components/PortfolioSection/ProjectData.js
+++ src/components/PortfolioSection/ProjectData.js
@@ -2,6 +2,12 @@ import spotify from "../../assets/images/spotify/spotifyclone.png";
 import rapcaviar from "../../assets/images/spotify/rapcaviar.png";
 import rapcaviar2 from "../../assets/images/spotify/rapcaviar2.png";
 
+import lightmode from "../../assets/images/lnb/lightmode.png";
+import darkmode from "../../assets/images/lnb/darkmode.png";
+import eventmediapost from "../../assets/images/lnb/eventmediapost.png";
+import userprofile from "../../assets/images/lnb/userprofile.png";
+import signin from "../../assets/images/lnb/signin.png";
+
 import amazon from "../../assets/images/amazon/home.png";
 import cart from "../../assets/images/amazon/cart.png";
 import checkout from "../../assets/images/amazon/checkout.png";
@@ -18,6 +24,8 @@ import contact from "../../assets/images/landingpage/contact.png";
 
 // TODO: Add LNB project
 
+export const darkscreen = darkmode;
+export const login = signin;
 export const projects = [
   {
     id: 1,
@@ -32,25 +40,24 @@ export const projects = [
   },
   {
     id: 2,
-    linkTo: "/amazon-clone",
-    name: "Amazon Clone",
-    stack: "React / Node / Express / Firebase Cloud Functions & Hosting",
+    linkTo: "",
+    name: "LNB",
+    stack: "React Native / Firebase ",
     description:
-      "A full stack Amazon clone with full e-commerce functionality, Stripe payment processsing, and user authentication.",
-    images: [amazon, cart, checkout],
-    url: "https://clone-7b53f.web.app/",
-    github: "https://github.com/anwachuku15/amazon-clone",
+      "A social networking app built with React Native (optimized for iOS). Implemented Firebase for full backend support, including for authentication, database management, and storage. Features real-time chat functionality and push notifications. This is app is currently being tested through Apple Test Flight.",
+    images: [lightmode, eventmediapost, userprofile],
+    url: "",
+    github: "https://github.com/anwachuku15/LNB",
   },
   {
     id: 3,
-    linkTo: "/facebook-clone",
-    name: "Facebook Clone",
-    stack: "MERN / Firebase Hosting / Heroku",
-    description:
-      "Facebook home page clone allowing users to create posts. Features Google user authentication.",
-    images: [facebook, facebook, facebook],
-    url: "https://facebookclone-mern-c56cd.firebaseapp.com/",
-    github: "https://github.com/anwachuku15/LNB",
+    linkTo: "/landingpage",
+    name: "Business Landing Page Clone",
+    stack: "React / Firebase Hosting",
+    description: "Business landing page clone with responsive web design.",
+    images: [carousel, services, contact],
+    url: "https://officium-f28bd.web.app/",
+    github: "https://github.com/anwachuku15/officium",
   },
   {
     id: 4,
@@ -65,12 +72,24 @@ export const projects = [
   },
   {
     id: 5,
-    linkTo: "/landingpage",
-    name: "Business Landing Page Clone",
-    stack: "React / Firebase Hosting",
-    description: "Business landing page clone with responsive web design.",
-    images: [carousel, services, contact],
-    url: "https://officium-f28bd.web.app/",
-    github: "https://github.com/anwachuku15/officium",
+    linkTo: "/amazon-clone",
+    name: "Amazon Clone",
+    stack: "React / Node / Express / Firebase Cloud Functions & Hosting",
+    description:
+      "A full stack Amazon clone with full e-commerce functionality, Stripe payment processsing, and user authentication.",
+    images: [amazon, cart, checkout],
+    url: "https://clone-7b53f.web.app/",
+    github: "https://github.com/anwachuku15/amazon-clone",
+  },
+  {
+    id: 6,
+    linkTo: "/facebook-clone",
+    name: "Facebook Clone",
+    stack: "MERN / Firebase Hosting / Heroku",
+    description:
+      "Facebook home page clone allowing users to create posts. Features Google user authentication.",
+    images: [facebook, facebook, facebook],
+    url: "https://facebookclone-mern-c56cd.firebaseapp.com/",
+    github: "https://github.com/anwachuku15/LNB",
   },
 ];
