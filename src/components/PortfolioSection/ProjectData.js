df89a047c18554a88d6a4f1b7d29549e02cce434
anwachuku15 <anwachuku15@gmail.com>
2020-12-10 08:11:32 -0500

    updated project data

--- src/components/PortfolioSection/ProjectData.js
+++ src/components/PortfolioSection/ProjectData.js
@@ -31,7 +31,7 @@ export const projects = [
     id: 1,
     linkTo: "/spotify-clone",
     name: "Spotify Clone",
-    stack: "React / Firebase Hosting",
+    stack: "React / Firebase",
     description:
       "A Spotify clone that allows existing Spotify users to play music from up to 50 of their most recent playlists. Built with React and deployed through Firebase Hosting.",
     images: [rapcaviar, rapcaviar2, spotify],
@@ -42,9 +42,9 @@ export const projects = [
     id: 2,
     linkTo: "",
     name: "LNB",
-    stack: "React Native / Firebase",
+    stack: "React Native (Expo) / React Navigation / Firebase",
     description:
-      "A social networking app built with React Native (optimized for iOS). Implemented Firebase for full backend support, including for authentication, database management, and storage. Features real-time chat functionality and push notifications. ",
+      "A social networking app built with React Native (optimized for iOS). Implemented Firebase for full backend support, including for authentication, database management, and storage. Features real-time updates, instant messaging, and push notifications. ",
     description2:
       "This is app is currently in beta testing via Apple Test Flight.",
     images: [lightmode, eventmediapost, userprofile],
@@ -55,9 +55,9 @@ export const projects = [
     id: 3,
     linkTo: "/amazon-clone",
     name: "Amazon Clone",
-    stack: "React / Node / Express / Firebase Cloud Functions & Hosting",
+    stack: "React / Node / Express / Firebase",
     description:
-      "A full stack Amazon clone with full e-commerce functionality, Stripe payment processsing, and user authentication.",
+      "A full stack Amazon clone with full e-commerce functionality including Stripe payment processsing. Implemented Firebase for user authentication, database management, hosting, and further backend logic with Node (Firebase Cloud Functions).",
     images: [amazon, cart, checkout],
     url: "https://clone-7b53f.web.app/",
     github: "https://github.com/anwachuku15/amazon-clone",
@@ -66,9 +66,9 @@ export const projects = [
     id: 4,
     linkTo: "/collabo",
     name: "Collabo",
-    stack: "React / Node / Express / Firebase Cloud Functions & Hosting",
+    stack: "React / Node / Express / Firebase",
     description:
-      "Twitter-esque app allowing authenticated users to create posts and receive notifications each time a post is liked or commented by other users.",
+      "Twitter-esque app allowing authenticated users to create posts and receive notifications each time a post is liked or commented by other users. Implemented Firebase for authentication, database management, hosting, and cloud functions.",
     images: [profile, post, signup],
     url: "https://social-3be8a.firebaseapp.com/",
     github: "https://github.com/anwachuku15/social-client",
@@ -77,8 +77,9 @@ export const projects = [
     id: 5,
     linkTo: "/landingpage",
     name: "Business Landing Page",
-    stack: "React / Firebase Hosting",
-    description: "Business landing page clone with responsive web design.",
+    stack: "React / Firebase",
+    description:
+      "Business landing page clone with responsive web design. Hosted through Firebase.",
     images: [carousel, services, contact],
     url: "https://officium-f28bd.web.app/",
     github: "https://github.com/anwachuku15/officium",
@@ -87,9 +88,9 @@ export const projects = [
     id: 6,
     linkTo: "/facebook-clone",
     name: "Facebook Clone",
-    stack: "MERN / Firebase Hosting / Heroku",
+    stack: "MERN / Heroku / Firebase",
     description:
-      "Facebook home page clone allowing users to create posts. Features Google user authentication.",
+      "Facebook home page clone allowing users to create posts. Features Google user authentication. Utilized Firebase for frontend hosting. Deployed the backend through Heroku.",
     images: [facebook, facebook, facebook],
     url: "https://facebookclone-mern-c56cd.firebaseapp.com/",
     github: "https://github.com/anwachuku15/LNB",
