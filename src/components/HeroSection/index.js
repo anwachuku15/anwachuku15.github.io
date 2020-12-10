4650188c94f25db03f8dfcd10b674bfd667cb89c
anwachuku15 <anwachuku15@gmail.com>
2020-12-10 08:09:58 -0500

    added animations; created linkedin link

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -7,9 +7,11 @@ import {
   HeroMainText,
   HeroH1,
 } from "./HeroElements";
+import "./HeroSection.css";
 import Button from "../Button";
 import Video from "../../assets/videos/herovideo.mp4";
 import ScrollAnimation from "react-animate-on-scroll";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 const HeroSection = () => {
   return (
@@ -18,35 +20,42 @@ const HeroSection = () => {
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
       <HeroContent>
-        <ScrollAnimation animateIn="zoomIn" duration={1.5} animateOnce={true}>
+        <ScrollAnimation animateIn="zoomIn" duration={1} animateOnce>
+          <HeroH1>Hello,</HeroH1>
+        </ScrollAnimation>
+        <ScrollAnimation
+          animateIn="zoomIn"
+          duration={1}
+          animateOnce
+          delay={100}
+        >
           <HeroMainText>
-            <HeroH1>Hello, I'm </HeroH1>
+            <HeroH1>I'm</HeroH1>
             <HeroH1
-              style={{
-                color: "rgb(225 21 21)",
-                background: "rgba(255, 255, 255, 0.69)",
-                padding: "1px 5px",
-                borderRadius: 10,
-                display: "inline-block",
-                marginLeft: 10,
-              }}
+              className="hero__h1 animate__animated"
+              onClick={() =>
+                window.open("https://www.linkedin.com/in/anwachuku15")
+              }
             >
               Andrew Nwachuku
+              <div className="hero__linkedIn">
+                <LinkedInIcon fontSize="large" />
+              </div>
             </HeroH1>
             <HeroH1>.</HeroH1>
           </HeroMainText>
         </ScrollAnimation>
         <ScrollAnimation
-          delay={100}
+          delay={500}
           animateIn="zoomInLeft"
-          duration={1.5}
+          duration={1}
           animateOnce={true}
         >
           <HeroH1>I'm a full-stack web developer.</HeroH1>
         </ScrollAnimation>
         <ScrollAnimation
-          animateIn="zoomIn"
-          delay={1300}
+          animateIn="rotateIn"
+          delay={1800}
           duration={1}
           animateOnce={true}
           style={{ alignSelf: "center" }}
