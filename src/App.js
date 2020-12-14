df49f87e02c05f0651b334ce72cb1446992d8c7f
anwachuku15 <anwachuku15@gmail.com>
2020-12-14 16:34:03 -0500

    conditional rendering location

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Helmet } from "react-helmet";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useLocation,
+} from "react-router-dom";
 
 import ResponsiveNavbar from "./components/ResponsiveNavbar";
 import Home from "./pages/Home";
@@ -9,20 +14,30 @@ import Project from "./pages/Project";
 import Footer from "./components/Footer";
 
 function App() {
+  let location = useLocation();
+  const [isNotFound, setIsNotFound] = useState(false);
+  useEffect(() => {
+    if (location.pathname !== "/") {
+      setIsNotFound(true);
+    } else {
+      setIsNotFound(false);
+    }
+  }, [location.pathname]);
+
   return (
-    <Router>
-      <div className="App">
-        <Helmet>
-          <meta name="author" content="Andrew Nwachuku" />
-        </Helmet>
-        <ResponsiveNavbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/:project" component={Project} />
-        </Switch>
-        <Footer />
-      </div>
-    </Router>
+    // <Router>
+    <div className="App">
+      <Helmet>
+        <meta name="author" content="Andrew Nwachuku" />
+      </Helmet>
+      <ResponsiveNavbar notFound={isNotFound} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/:project" component={Project} />
+      </Switch>
+      <Footer />
+    </div>
+    // </Router>
   );
 }
 
