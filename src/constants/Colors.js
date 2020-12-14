5b8c4746e63f38acfc9db3e3493a1892d82fa341
anwachuku15 <anwachuku15@gmail.com>
2020-12-14 12:17:21 -0500

    added Colors constants

---
+++ src/constants/Colors.js
@@ -0,0 +1,13 @@
+export default {
+  maroondark: "#2e1c1c",
+  primaryRed: "#d23f3f",
+  secondaryRed: "rgba(210,63,63,1)",
+  lightRed: "#ffefef",
+  lightRed2: "#FF5A5A",
+  black: "#060101",
+  darkNavy: "#181625",
+  navy: "#322f4e",
+  linkedIn: "#0A66C2",
+  googleRed: "#EA4335",
+  googleGreen: "#34A853",
+};
